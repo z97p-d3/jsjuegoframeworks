819a07251d8062a79816fe190bd68d204cbdcb8b
Dario Pilacuan <dario.pilacuan@epn.edu.ec>
2019-09-14 14:48:53 -0500

    load

--- js/app.js
+++ js/app.js
@@ -1,2 +1,37 @@
-var color =$('.main-titulo').css("color", "red");
+$(function () {
+	//cambiar color a titulo pricipal 
+	function animartitulo() {
+		$(".main-titulo").animate({
+				color: 'red'
+			}, "slow")
+			.animate({
+				color: 'yellow'
+			}, "slow")
+			.animate({
+				color: 'white'
+			}, "fast")
 
+
+	}
+
+	setInterval(animartitulo, 500);
+
+	$(document).click(function () {
+			$(".panel-score").animate({
+					width: "100%",
+					height: 700
+
+				}), 2000, function () {
+
+					$(".panel-tablero").hide("slow");
+				};
+
+
+	});
+
+
+
+
+
+
+});
